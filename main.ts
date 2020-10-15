cf409714931e4c05b5183cdb77a739d727334860
scratchyone <scratchywon@gmail.com>
2020-10-15 18:13:26 -0400

    Make client stop attempting to fetch deleted messages

--- main.ts
+++ main.ts
@@ -2226,16 +2226,6 @@ client.on('guildMemberAdd', async (member) => {
 client.on(
   'messageDelete',
   async (msg: Discord.Message | Discord.PartialMessage) => {
-    if (msg.partial) {
-      // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-      try {
-        await msg.fetch();
-      } catch (error) {
-        console.log('Something went wrong when fetching the message: ', error);
-        // Return as `reaction.message.author` may be undefined/null
-        // return;
-      }
-    }
     const message = msg as util_functions.EMessage;
     const bm = await Types.BotMessage.query().where('message', msg.id);
     if (
@@ -2250,31 +2240,6 @@ client.on(
         }
       } catch (e) {
         console.error(e);
-      }
-    }
-  }
-);
-client.on(
-  'messageDeleteBulk',
-  async (
-    msgs: Discord.Collection<
-      Discord.Snowflake,
-      Discord.Message | Discord.PartialMessage
-    >
-  ) => {
-    for (const msg of msgs.array()) {
-      if (msg.partial) {
-        // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-        try {
-          await msg.fetch();
-        } catch (error) {
-          console.log(
-            'Something went wrong when fetching the message: ',
-            error
-          );
-          // Return as `reaction.message.author` may be undefined/null
-          // return;
-        }
       }
     }
   }
