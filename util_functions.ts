3bfedcfd7dd66c80edb03a6a3673d36892a932f5
scratchyone <scratchywon@gmail.com>
2022-07-07 19:03:34 -0400

    Fix BigInt serialization

--- util_functions.ts
+++ util_functions.ts
@@ -581,3 +581,13 @@ export function chunk<T>(arr: T[], len: number): T[][] {
 
   return chunks;
 }
+// Make BigInt serializable
+// eslint-disable-next-line @typescript-eslint/no-redeclare, no-unused-vars
+interface BigInt {
+  /** Convert to BigInt to string form in JSON.stringify */
+  toJSON: () => string;
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(BigInt.prototype as any)['toJSON'] = function () {
+  return this.toString();
+};
